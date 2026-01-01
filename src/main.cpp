10b8243885e7119052d6cb1060a873a6b3a408c1
Tyler Hoover <80370906+tjayhoover@users.noreply.github.com>
2026-01-01 15:51:22 -0500

    Simple main function to test my engine and compute first order sensitivies for a simple expression.

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,32 @@
 
 #include "node.h"
 #include "tape.h"
+#include "variable_data.h"
 #include "variable.h"
+#include "operators.h"
 
 using namespace std;
 
-int main() {
-    Tape tape = Tape();
+int main() { 
+    // Create the input variables
+    Variable x = Variable(3.0);
+    Variable y = Variable(4.0);
 
-    cout << "Number of nodes in tape: " << tape.nodes.size() << endl;
-    
-    Variable x = Variable();
-    Variable y = Variable();
+    // Write the expression (implicitly builds a computation graph)
+    Variable z = sin(x + y) * exp(x / y);
 
-    x.tape = &tape;
-    y.tape = &tape;
+    cout << "Number of nodes in tape after operation: " << Tape::get().nodes.size() << endl;
 
-    x.value = 3.0;
-    y.value = 4.0;
-    auto result = x * y;
+    // Set the adjoint of the output variable to 1.0 to start backpropagation
+    z.setAdjoint(1.0);
 
-    cout << result << endl;
+    // Execute the backward pass to compute the gradients
+    Tape::get().execute_backpass();
 
-    cout << "Number of nodes in tape after operation: " << tape.nodes.size() << endl;
+    cout << "Adjoints:" << endl;
+    cout << "dz/dx: " << x.adjoint() << endl;
+    cout << "dz/dy: " << y.adjoint() << endl;
 
-    Node* node = tape.nodes[0];
-    cout << "Number of inputs in the first node: " << node->inputs.size() << endl;
-    node->backward_function();
-
+    cout << "Number of inputs in the first node: " << Tape::get().nodes[0]->inputs.size() << endl;
     return 0;
 }
